d1fbd669a84b889942d03205c7790e6e397089a4
hrebeniev <nick.hrebeniev@gmail.com>
2023-03-28 21:08:51 +0300

    Homework 10

---
+++ src/main/java/com/hillel/hrebeniev/homeworks/homework_10/MathLibrarys.java
@@ -0,0 +1,11 @@
+package com.hillel.hrebeniev.homeworks.homework_10;
+
+import com.hillel.hrebeniev.MathLibrary;
+
+public class MathLibrarys {
+    public static void main(String[] args) {
+        MathLibrary mathLibrary = new MathLibrary(1,3);
+        System.out.println(mathLibrary);
+    }
+
+}
