260e38a0fba5ca3901b3e4529899a6fbea9b92d2
hrebeniev <nick.hrebeniev@gmail.com>
2023-04-08 10:53:13 +0300

    Homework 13

---
+++ src/test/java/SorterTest.java
@@ -0,0 +1,36 @@
+import com.hillel.hrebeniev.homeworks.homework_13.Sorter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SorterTest {
+
+    @Test
+    public void quickSortTest() {
+        int[] arr1 = {5, 3, 7, 1, 8, 2, 6, 4};
+        int[] sortedArr1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr1, Sorter.quickSort(arr1));
+
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr2, Sorter.quickSort(arr2));
+
+        int[] arr3 = {8, 7, 6, 5, 4, 3, 2, 1};
+        int[] sortedArr3 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr3, Sorter.quickSort(arr3));
+    }
+
+    @Test
+    public void cocktailSort() {
+        int[] arr1 = {5, 3, 7, 1, 8, 2, 6, 4};
+        int[] sortedArr1 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr1, Sorter.quickSort(arr1));
+
+        int[] arr2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        int[] sortedArr2 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr2, Sorter.quickSort(arr2));
+
+        int[] arr3 = {8, 7, 6, 5, 4, 3, 2, 1};
+        int[] sortedArr3 = {1, 2, 3, 4, 5, 6, 7, 8};
+        Assertions.assertArrayEquals(sortedArr3, Sorter.quickSort(arr3));
+    }
+}
